649ab6913c5944b4d99bb466fa8eac7df5f64cba
Kevin Tassi <kevin@fishtripr.com>
2019-03-07 14:44:50 +0100

    add image container componenet

---
+++ src/components/image-container/ImageContainer.vue
@@ -0,0 +1,54 @@
+<template>
+  <div 
+    class="image-container"
+    :style='{ 
+    backgroundImage: coverImg,
+    paddingTop: verticalSize
+    }'>
+
+    <slot/>
+
+  </div>
+</template>
+
+<script>
+import { FALLBACK_FISHTRIPR_COVER } from '../../constants/fallbacks'
+
+export default {
+  name: 'image-container',
+  props: {
+    image: {
+      type: String,
+      default: FALLBACK_FISHTRIPR_COVER
+    },
+    height: {
+      type: Number,
+      default: 40,
+      validator: value => {
+        return typeof value === "number" && value > 1
+      }
+    }
+  },
+  computed: {
+    coverImg() {
+      return `url('${this.image}')`
+    },
+    verticalSize() {
+      return `${this.height}%`
+    }
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+.image-container {
+  display: flex;
+  overflow: hidden;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  position: relative;
+  width: 100%;
+  min-width: 100px;
+}
+</style>
---
+++ src/constants/fallbacks.js
@@ -0,0 +1,1 @@
+export const FALLBACK_FISHTRIPR_COVER = "https://fishtripr-prod-offer-pics.s3.eu-west-3.amazonaws.com/fallbacks/cover.jpeg";
