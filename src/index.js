be19580e9f991f466633b4748c445b662cca470f
Kevin Tassi <kevin@fishtripr.com>
2019-03-13 13:26:57 +0100

    fix(index): add export module for Vue

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
 import Avatar from './components/avatar';
 import ImageContainer from './components/image-container';
 
-export {
+const components = [
   Avatar,
   ImageContainer,
+];
+
+const install = function(Vue) {
+  components.map(component => {
+    Vue.component(component.name, component);
+  });
 };
+
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue);
+}
+
+module.exports = {
+  install,
+  Avatar,
+  ImageContainer
+};
+
+module.exports.default = module.exports;
