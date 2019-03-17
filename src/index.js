61a76eeba6eeaabf9d4f85654380a0e213bbe162
Kevin Tassi <kevin@fishtripr.com>
2019-03-17 14:13:10 +0100

    remove comments

--- src/index.js
+++ src/index.js
@@ -2,19 +2,6 @@ import Vue from 'vue'
 import Avatar from './components/avatar';
 import ImageContainer from './components/image-container';
 
-// const Components = {
-//   Avatar,
-//   ImageContainer
-// }
-
-// export default {
-//   install(Vue, options) {
-//     Object.keys(Components).forEach((name) => {
-//       Vue.component(name, Components[name])
-//     })
-//   }
-// }
-
 export {
   Avatar,
   ImageContainer,
