ec0d4800131499964cdf153adbced48a764b9f55
Kevin Tassi <kevin@fishtripr.com>
2019-07-03 09:38:16 +0200

    Expose base-button components

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ImageContainer from './components/image-container';
 import OfferCard from './components/cards/offer-card'
 import ChatMessageCard from './components/cards/chat-message-card';
 import AppCard from './components/cards/app-card';
+import BaseButton from './components/button';
 
 export {
   Avatar,
   ImageContainer,
   OfferCard,
   ChatMessageCard,
-  AppCard
+  AppCard,
+  BaseButton
 }
