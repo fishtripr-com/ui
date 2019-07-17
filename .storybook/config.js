cdee5e1a1ed0e3ce91bd1f621d1ad8971df0a942
IgorZ07 <wow0707@gmail.com>
2019-07-17 16:02:07 +0200

    Config file changed

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,34 +1,36 @@
-import { configure } from '@storybook/vue';
+import { configure } from "@storybook/vue";
 
-import Vue from 'vue';
+import Vue from "vue";
 
 // Import your custom components.
-import Avatar from '../src/components/avatar';
-import ImageContainer from '../src/components/image-container';
-import ConversationCard from '../src/components/cards/conversation-card';
-import FishtriprLoader from '../src/components/loaders/fishtripr-loader';
-import OfferCard from '../src/components/cards/offer-card';
-import ChatMessageCard from '../src/components/cards/chat-message-card';
-import AppCard from '../src/components/cards/app-card';
-import EmployeeCard from '../src/components/cards/employee-card';
-import UserNavbarCard from '../src/components/cards/user-navbar-card';
-import BaseButton from '../src/components/button';
+import Avatar from "../src/components/avatar";
+import ImageContainer from "../src/components/image-container";
+import ConversationCard from "../src/components/cards/conversation-card";
+import FishtriprLoader from "../src/components/loaders/fishtripr-loader";
+import OfferCard from "../src/components/cards/offer-card";
+import ChatMessageCard from "../src/components/cards/chat-message-card";
+import AppCard from "../src/components/cards/app-card";
+import EmployeeCard from "../src/components/cards/employee-card";
+import UserNavbarCard from "../src/components/cards/user-navbar-card";
+import BaseButton from "../src/components/button";
+import MarketingThumbsCard from "../src/components/cards/marketing-thumbs-card";
 
 // Register custom components.
-Vue.component('avatar', Avatar);
-Vue.component('image-container', ImageContainer);
-Vue.component('conversation-card', ConversationCard);
-Vue.component('fishtripr-loader', FishtriprLoader);
-Vue.component('offer-card', OfferCard);
-Vue.component('chat-message-card', ChatMessageCard);
-Vue.component('app-card', AppCard);
-Vue.component('employee-card', EmployeeCard);
-Vue.component('user-navbar-card', UserNavbarCard);
-Vue.component('base-button', BaseButton);
+Vue.component("avatar", Avatar);
+Vue.component("image-container", ImageContainer);
+Vue.component("conversation-card", ConversationCard);
+Vue.component("fishtripr-loader", FishtriprLoader);
+Vue.component("offer-card", OfferCard);
+Vue.component("chat-message-card", ChatMessageCard);
+Vue.component("app-card", AppCard);
+Vue.component("employee-card", EmployeeCard);
+Vue.component("user-navbar-card", UserNavbarCard);
+Vue.component("base-button", BaseButton);
+Vue.component("marketing-thumbs-card", MarketingThumbsCard);
 
 function loadStories() {
   // You can require as many stories as you need.
-  require('../stories');
+  require("../stories");
 }
 
 configure(loadStories, module);
