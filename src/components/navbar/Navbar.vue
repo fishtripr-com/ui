3164b489901affb1506826c0cd2689958c0c4b58
IgorZ07 <wow0707@gmail.com>
2019-08-12 18:21:01 +0200

    Responsiveness added with the help of mixins, colors changing in line with theme

--- src/components/navbar/Navbar.vue
+++ src/components/navbar/Navbar.vue
@@ -1,25 +1,16 @@
 <template>
   <div :class="['navbar', theme]">
-
-    <div class="wrapper">
-
-      <div 
-        :class="['logo', theme]"
-        @click="logoClicked" 
-        >
+    <div :class="['wrapper', theme]">
+      <div :class="['logo', theme, deviceSize < deviceSizes.l ? 'mobile': '']" @click="logoClicked">
         <img class="image" :src="logoUrl" alt="logo" />
       </div>
 
-      <div :class="['body', theme]">
-
-        <slot name="body"/>
-
+      <div :class="['body', theme, deviceSize < deviceSizes.l ? 'mobile': '']">
+        <slot name="body" :theme="theme" />
       </div>
 
-      <div class="actions">
-
-        <slot name="actions"/>
-
+      <div :class="['actions', theme]">
+        <slot name="actions" :theme="theme" />
       </div>
 
     </div>
@@ -28,16 +19,18 @@
 </template>
 
 <script>
+import { responsiveHandler } from "../../mixins/responsiveHandler";
+
 export default {
-  name: 'navbar',
+  name: "navbar",
+  mixins: [responsiveHandler],
   props: {
     theme: {
       type: String,
       default: 'light',
       validator: value => ['dark', 'premium', 'light'].includes(value)
     },
-    logoUrl: { type: String },
-    mobile: { type: Boolean, default: false }
+    logoUrl: { type: String }
   },
  
   methods: {
@@ -96,6 +89,37 @@ export default {
 
     .body {
       flex-grow: 1;
+
+      &.light {
+        border-left: 1px solid $steel;
+      }
+      &.dark {
+        border-left: 1px solid $steel;
+      }
+      &.premium {
+        border-left: 1px solid #958ce4;
+      }
+
+      &.mobile {
+        // order: 3;
+        position: absolute;
+
+        top: $height-navbar + 8px;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        border-left: 0;
+
+        &.light {
+          border-top: 1px solid $steel;
+        }
+        &.dark {
+          border-top: 1px solid $steel;
+        }
+        &.premium {
+          border-top: 1px solid #958ce4;
+        }
+      }
     }
     .actions {
       margin-left: 20px;
--- stories/navbar/index.js
+++ stories/navbar/index.js
@@ -8,8 +8,9 @@ storiesOf('Navbar', module)
       theme="light"
       logoUrl="https://picsum.photos/150/80">
 
-        <template #body>
+        <template #body="{theme}">
           <navigation
+            :theme="theme"
             :items="[{id:0, 'path':'/aaa', 'label':'Home'},
               {id:1, 'path':'/bbb', 'label':'About'},
               {id:2, 'path':'/ccc', 'label':'Contact'},
@@ -17,8 +18,9 @@ storiesOf('Navbar', module)
           />
         </template>
 
-        <template #actions>
+        <template #actions="{theme}">
           <user-navbar-card
+            :theme="theme"
             avatar="https://s.gravatar.com/avatar/a3895a2d6f26155968be47fc03dddc40?s=80"
             user-first-name="Max"
             user-role="Fishing Guide"
@@ -27,26 +29,60 @@ storiesOf('Navbar', module)
 
       </navbar>`
   )
-  // .add('Navbar Dark', () =>
-  //   `<navbar 
-  //   theme="dark"
-  //     logo="https://s3.eu-west-3.amazonaws.com/fishtripr-prod-offer-pics/images/logos/fishtripr-manager-only-manager-logo_white.png">
-  //       <template #body>
-  //       <navigation-menu 
-  //       isSmallScreenMode
-  //       :list="[{id:0, 'path':'/aaa', 'label':'Home'},
-  //         {id:1, 'path':'/bbb', 'label':'About'},
-  //         {id:2, 'path':'/ccc', 'label':'Contact'},
-  //         {id:3, 'path':'/ddd', 'label':'Information'}]"
-  //     />
-  //     </template>
-  //       <template #action>
-  //         <user-navbar-card
-  //           avatar="https://s.gravatar.com/avatar/a3895a2d6f26155968be47fc03dddc40?s=80"
-  //           user-first-name="Max"
-  //           user-role="Fishing Guide"
-  //         />
-  //       </template>
-  //     </navbar>`
-  // );
- 
+  .add(
+    'Dark',
+    () =>
+      `<navbar 
+      theme="dark"
+      logoUrl="https://picsum.photos/150/80">
+
+        <template #body="{theme}">
+          <navigation
+            :theme="theme"
+            :items="[{id:0, 'path':'/aaa', 'label':'Home'},
+              {id:1, 'path':'/bbb', 'label':'About'},
+              {id:2, 'path':'/ccc', 'label':'Contact'},
+              {id:3, 'path':'/ddd', 'label':'Information'}]"
+          />
+        </template>
+
+        <template #actions="{theme}">
+          <user-navbar-card
+            :theme="theme"
+            avatar="https://s.gravatar.com/avatar/a3895a2d6f26155968be47fc03dddc40?s=80"
+            user-first-name="Max"
+            user-role="Fishing Guide"
+          />
+        </template>
+
+      </navbar>`
+  )
+  .add(
+    'Premium',
+    () =>
+      `<navbar 
+      theme="premium"
+      logoUrl="https://picsum.photos/150/80">
+
+        <template #body="{theme}">
+          <navigation
+            :theme="theme"
+            :items="[{id:0, 'path':'/aaa', 'label':'Home'},
+              {id:1, 'path':'/bbb', 'label':'About'},
+              {id:2, 'path':'/ccc', 'label':'Contact'},
+              {id:3, 'path':'/ddd', 'label':'Information'}]"
+          />
+        </template>
+
+        <template #actions="{theme}">
+          <user-navbar-card
+            :theme="theme"
+            avatar="https://s.gravatar.com/avatar/a3895a2d6f26155968be47fc03dddc40?s=80"
+            user-first-name="Max"
+            user-role="Fishing Guide"
+          />
+        </template>
+
+      </navbar>`
+  );
+
