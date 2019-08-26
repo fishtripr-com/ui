554d9fbb3e950f70600cc2413a5f8d292de78ac0
Kevin Tassi <kevin.tassi@gmail.com>
2019-08-26 17:30:24 +0200

    Config build webpack

--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,18 @@ const config = {
         test: /\.scss$/,
         include: [SRC_DIR],
         loaders: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
       }
     ],
   },
