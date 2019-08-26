0b471d168daa649740f1eb1bbe7cb9818c51b638
Kevin Tassi <kevin.tassi@gmail.com>
2019-08-26 17:28:50 +0200

    Add ShortExperiencePreview component

---
+++ stories/experience/short-experience-preview/index.js
@@ -0,0 +1,16 @@
+import { storiesOf } from '@storybook/vue';
+
+storiesOf('Short Experience Preview', module)
+  .add(
+    'Default',
+    () =>
+      `<div style="width:350px;">
+        <short-experience-preview
+          title="Fishing with father and son"
+          country="Belgium"
+          image="https://picsum.photos/200/300"
+          is-listed
+        />
+      </div>`
+  );
+
