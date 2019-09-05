1646fb4302d204bef85bad0c3a88d3ddf364633e
IgorZ07 <wow0707@gmail.com>
2019-09-05 14:18:52 +0200

    Story created

--- .storybook/config.js
+++ .storybook/config.js
@@ -22,6 +22,7 @@ import Navigation from '../src/components/navigation';
 import ExperienceCard from '../src/components/cards/experience-card';
 import PartnerCard from '../src/components/cards/partner-card';
 import ShortExperiencePreview from '../src/components/experience/short-experience-preview/ShortExperiencePreview';
+import MobileOfferHeader from '../src/components/mobile-offer-header';
 
 // Register custom components.
 Vue.component('avatar', Avatar);
@@ -43,6 +44,7 @@ Vue.component('navigation', Navigation);
 Vue.component('experience-card', ExperienceCard);
 Vue.component('partner-card', PartnerCard);
 Vue.component('short-experience-preview', ShortExperiencePreview);
+Vue.component('mobile-offer-header', MobileOfferHeader);
 
 function loadStories() {
   // You can require as many stories as you need.
