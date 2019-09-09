e1db7dc9c29443729ebe98d24b950a62343766a9
Kevin Tassi <kevin.tassi@gmail.com>
2019-09-09 15:51:18 +0200

    Test to writ story with object instead pure template

--- stories/experience/short-experience-preview/index.js
+++ stories/experience/short-experience-preview/index.js
@@ -3,13 +3,19 @@ import { storiesOf } from '@storybook/vue';
 storiesOf('Experience/Short Experience Preview', module)
   .add(
     'Default',
-    () =>
-      `<div style="width:350px;">
-        <short-experience-preview
-          title="Fishing with father and son"
-          country="Belgium"
-          image="https://picsum.photos/200/300"
-          is-listed
-        />
-      </div>`
+    () => ({
+      data() {
+        return {
+          title: "Fishing with father and son"
+        }
+      },
+      template: `<div style="width:350px;">
+      <short-experience-preview
+        :title="title"
+        country="Belgium"
+        image="https://picsum.photos/200/300"
+        is-listed
+      />
+    </div>`
+    })
   )
