cc29fa6eaefbb0b9de05cd260c1b6da7f91df5da
Kevin Tassi <kevin.tassi@gmail.com>
2019-09-09 17:15:00 +0200

    Refactor the script to publish on npm

---
+++ npm-publisher-patch.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Get the name of the current Git branch and then ensure that only
+# the master Git branch is deployable to NPM by this script.
+branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+
+# Only push this update to the NPM if the current branch is the master branch
+if [ "$branch" == "master" ]
+then
+  echo "Building and deploying the $branch branch to NPM..."
+
+  yarn build:prod && npm version patch && npm publish --access public
+
+  echo "Finished deploying to NPM."
+else
+  echo "Will not publish from a branch other than master to NPM. Check out master to deploy to NPM"
+fi
