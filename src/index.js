58f1d1b5550b068f9a22b6d1f39c9ba5a8e0c405
Kevin Tassi <kevin.tassi@gmail.com>
2019-09-10 11:02:44 +0200

    Get rid of semicolon and coment

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import AppCard from './components/cards/app-card';
-import Avatar from './components/avatar';
-import BaseButton from './components/button';
-import ChatMessageCard from './components/cards/chat-message-card';
-import CheckBoxCard from './components/check-box-card';
+import AppCard from './components/cards/app-card'
+import Avatar from './components/avatar'
+import BaseButton from './components/button'
+import ChatMessageCard from './components/cards/chat-message-card'
+import CheckBoxCard from './components/check-box-card'
 import ExperienceListingCard from './components/experience/experience-listing-card'
-import ImageContainer from './components/image-container';
-import MarketingThumb from './components/marketing-thumb';
-import OfferCard from './components/offer-card';
-import TipCard from './components/tip-card';
+import ImageContainer from './components/image-container'
+import MarketingThumb from './components/marketing-thumb'
+import OfferCard from './components/offer-card'
+import TipCard from './components/tip-card'
 import ConversationCard from './components/conversation/conversation-card'
 
 export {
@@ -17,13 +17,9 @@ export {
   ChatMessageCard,
   CheckBoxCard,
   ConversationCard,
-  // EmployeeCard,
   ExperienceListingCard,
-  // FishtriprLoader,
   ImageContainer,
   MarketingThumb,
   OfferCard,
-  // UserNavbarCard,
-  // TextTypeBloc,
   TipCard,
 }
