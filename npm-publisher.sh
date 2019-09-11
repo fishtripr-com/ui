9abca61ab052c8ee0edbd1585be6a8be44451562
Kevin Tassi <kevin.tassi@gmail.com>
2019-09-11 09:40:24 +0200

    Add parameter for patch, minor of major

--- npm-publisher.sh
+++ npm-publisher.sh
@@ -4,12 +4,19 @@
 # the master Git branch is deployable to NPM by this script.
 branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 
+updateType=$1
+
+if [[ "$updateType" == "" ]]
+then
+    updateType="patch"
+fi
+
 # Only push this update to the NPM if the current branch is the master branch
 if [ "$branch" == "master" ]
 then
   echo "Building and deploying the $branch branch to NPM..."
 
-  yarn build:prod && npm version patch && npm publish --access public
+  yarn build:prod && npm version $updateType && npm publish --access public
 
   echo "Finished deploying to NPM."
 else
