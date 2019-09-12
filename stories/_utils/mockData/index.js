a658c75102b77f623ec61f773e2b4a3de1bd8509
Kevin Tassi <kevin.tassi@gmail.com>
2019-09-12 10:04:46 +0200

    Add booking mock data

--- stories/_utils/mockData/index.js
+++ stories/_utils/mockData/index.js
@@ -8,5 +8,23 @@ export default {
     hat: '1 Guest - Jun 07, 2019 - €200',
     status: 'Pending',
     mode: 'warning'
+  },
+  booking: {
+    startDate: '28 Jun, 2019',
+    endDate: '31 Jun, 2019',
+    source: 'marketplace',
+    experienceCover: 'https://picsum.photos/200/300',
+    experienceTitle: 'An unforgettable father-and-son fishing experience in Jura',
+    experienceAddress: 'Rue Haute 4, 6990 Hotton, Belgique',
+    guests: '3',
+    duration: '2',
+    conversationId: 'ada72f55-fdae-4ba8-ad80-b65c42458d49',
+    customerFullName: 'Kevin Tassi',
+    customerEmail: 'info@fishtripr.com',
+    statusText: 'Accepted',
+    statusType: 'success',
+    totalPrice: '350',
+    deposit: '0',
+    currency: 'EUR'
   }
 }
