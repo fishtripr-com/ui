6b37de8c67f90d4d51b7b3a8da95d28ce9bf09a1
IgorZ07 <wow0707@gmail.com>
2019-09-16 12:14:07 +0200

    Created base of the component

---
+++ src/components/mobile-dashboard-header/MobileDashboardHeader.vue
@@ -0,0 +1,45 @@
+<template>
+  <header class="mobile-dashboard-header">
+    <div class="wrapper">
+      <p>Hello11</p>
+      <span>{{ text1 }}</span>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "mobile-dashboard-header",
+  props: {
+    text: { type: String, default: "" },
+    revenue: { type: Number, default: "0" },
+    currency: { type: String, default: "EUR" }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../../assets/style/main";
+
+.mobile-dashboard-header {
+  display: flex;
+  flex-flow: column;
+  font-family: $font-family;
+  font-size: $font-size-xs;
+  font-weight: $semibold;
+  color: $watermelon;
+
+  .wrapper {
+    display: flex;
+    flex-direction: row;
+    font-size: $font-size-xl;
+    font-weight: $bold;
+    color: $charcoal-grey;
+    margin-top: $space-xxs;
+  }
+
+  p {
+    margin: 0;
+  }
+}
+</style>
