1fdc4dc756006e606dd8731c9b27a3da297bbc89
IgorZ07 <wow0707@gmail.com>
2019-09-16 13:22:53 +0200

    Styles corrected

--- src/components/mobile-dashboard-header/MobileDashboardHeader.vue
+++ src/components/mobile-dashboard-header/MobileDashboardHeader.vue
@@ -55,20 +55,13 @@ export default {
   flex-flow: column;
   font-family: $font-family;
   font-size: $font-size-xs;
-  font-weight: $semibold;
-  color: $watermelon;
+  font-weight: $normal;
+  color: $charcoal-grey;
+}
 
-  .wrapper {
-    display: flex;
-    flex-direction: row;
-    font-size: $font-size-xl;
-    font-weight: $bold;
-    color: $charcoal-grey;
-    margin-top: $space-xxs;
-  }
-
-  p {
-    margin: 0;
-  }
+p {
+  margin: 0 0 $space-xxxs 0;
+  font-size: $font-size-l;
+  font-weight: $bold;
 }
 </style>
