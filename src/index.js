2436e7f97dc2c41888ea5b42b4abf55808fac12f
Kevin <kevin.tassi@gmail.com>
2019-09-16 17:16:44 +0200

    Rename the exposed object name

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import ImageContainer from './components/image-container'
 import MarketingThumb from './components/marketing-thumb'
 import OfferCard from './components/offer-card'
 import TipCard from './components/tip-card'
-import RevenueGraph from './components/revenue-graph'
+import InsigthGraph from './components/insight-graph'
 
 export {
   AppCard,
@@ -23,5 +23,5 @@ export {
   MarketingThumb,
   OfferCard,
   TipCard,
-  RevenueGraph
+  InsigthGraph
 }
