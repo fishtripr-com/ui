e8fceada26179953ec36e09b783b63a6efd06951
IgorZ07 <wow0707@gmail.com>
2019-09-16 13:21:37 +0200

    Added currency symbol change method

--- src/components/mobile-dashboard-header/MobileDashboardHeader.vue
+++ src/components/mobile-dashboard-header/MobileDashboardHeader.vue
@@ -12,6 +12,31 @@ export default {
     text: { type: String, default: "" },
     revenue: { type: Number, default: 0 },
     currency: { type: String, default: "EUR" }
+  },
+  methods: {
+    currencySymbol() {
+      const currency_symbols = {
+        USD: "$", // US Dollar
+        EUR: "€", // Euro
+        CRC: "₡", // Costa Rican Colón
+        GBP: "£", // British Pound Sterling
+        ILS: "₪", // Israeli New Sheqel
+        INR: "₹", // Indian Rupee
+        JPY: "¥", // Japanese Yen
+        KRW: "₩", // South Korean Won
+        NGN: "₦", // Nigerian Naira
+        PHP: "₱", // Philippine Peso
+        PLN: "zł", // Polish Zloty
+        PYG: "₲", // Paraguayan Guarani
+        THB: "฿", // Thai Baht
+        UAH: "₴", // Ukrainian Hryvnia
+        VND: "₫" // Vietnamese Dong
+      };
+
+      if (currency_symbols[this.currency] !== undefined) {
+        return currency_symbols[this.currency];
+      }
+    },
   }
 };
 </script>
