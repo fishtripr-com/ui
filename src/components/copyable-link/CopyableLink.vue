1f0d4b70447b2e44ffc76a10c1248f881df8170b
Kevin <kevin.tassi@gmail.com>
2019-09-17 14:41:21 +0200

    Update UX and switch to copied when link is copied

--- src/components/copyable-link/CopyableLink.vue
+++ src/components/copyable-link/CopyableLink.vue
@@ -1,10 +1,28 @@
 <template>
   <div class="copyable-link">
-    <header class="wrapper">
-      <span class="header-text">{{ label }}</span>
-      <span class="copy-button" @click.stop.prevent="copyLink">Copy</span>
+    <header class="wrapper-header mb-5">
+      <span class="header-text txt-4 semibold">{{ label }}</span>
+      <span
+        v-if="!isCopied"
+        class="copy-button txt-4 semibold premium"
+        @click.stop.prevent="copyLink"
+        >
+        Copy
+      </span>
+      <span
+        v-else
+        class="copy-button txt-4 semibold success"
+        >
+        Copied!
+      </span>
     </header>
-    <a class="link" :href="link">{{ link }}</a>
+    <a
+      class="link-to-copy txt-4 link-premium"
+      :href="link"
+      @click.stop.prevent="copyLink"
+      >
+      {{ link }}
+    </a>
     <input type="hidden" id="linkToCopy" :value="link" />
   </div>
 </template>
@@ -16,7 +34,15 @@ export default {
     label: { type: String, required: true },
     link: { type: String, required: true }
   },
+  data() {
+    return {
+      isCopied: false
+    }
+  },
   methods: {
+    switchIsCopied() {
+      this.isCopied = !this.isCopied
+    },
     copyLink() {
       const textToCopy = document.querySelector('#linkToCopy')
       textToCopy.setAttribute('type', 'text')
@@ -24,31 +50,31 @@ export default {
       document.execCommand('copy')
       textToCopy.setAttribute('type', 'hidden')
       window.getSelection().removeAllRanges()
+      this.switchIsCopied()
+      setTimeout(() => {
+        this.switchIsCopied()
+      }, 2000)
     }
   }
-};
+}
 </script>
 
 <style lang="scss" scoped>
-@import "../../assets/style/main";
+@import "../../assets/style/default";
 
 .copyable-link {
   display: flex;
   flex-direction: column;
-  font-family: $font-family;
-  font-size: $font-size-s;
 
-  .wrapper {
+  .wrapper-header {
     display: flex;
     flex-flow: row nowrap;
     justify-content: space-between;
-    margin-bottom: $space-m;
-    font-weight: $semibold;
-    color: $color-text;
 
-    .copy-button {
-      color: $color-text-info;
-    }
+    .copy-button { cursor: pointer }
+  }
+  .link-to-copy {
+    overflow-wrap: break-word;
   }
 }
 </style>
