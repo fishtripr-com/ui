a1084c38f8a3f26714748a8f255130b724596970
Kevin <kevin.tassi@gmail.com>
2019-09-26 15:21:27 +0200

    Optimise the story structure

--- stories/ui/navigation.js
+++ stories/ui/navigation.js
@@ -2,42 +2,42 @@ import { storiesOf } from '@storybook/vue'
 
 import data from '../_utils/mockData/index'
 
+const dataMixin = {
+  data() {
+    return {
+      navItems: data.nav.navItems
+    }
+  },
+}
+
 storiesOf('UI/Navigation/Theme', module)
 .add(
   'Light',
   () => ({
-    data() {
-      return {
-        navItems: data.nav.navItems
-      }
-    },
+    mixins: [dataMixin],
     template: `<navigation
       theme="light"
       :items="navItems"
       />`,
   })
 )
-
-
-// .add('Dark', () => {
-//   return {
-//     template: `<navigation
-//       theme="dark"
-//       :items="[
-//         {'id': 0, 'path': '/aaa', 'label': 'Home'},
-//         {'id': 1, 'path': '/bbb', 'label': 'About'},
-//       ]"
-//       />`,
-//   }
-// })
-// .add('Premium', () => {
-//   return {
-//     template: `<navigation
-//       theme="premium"
-//       :items="[
-//         {'id': 0, 'path': '/aaa', 'label': 'Home'},
-//         {'id': 1, 'path': '/bbb', 'label': 'About'},
-//       ]"
-//       />`,
-//   }
-// })
+.add(
+  'Dark',
+  () => ({
+    mixins: [dataMixin],
+    template: `<navigation
+      theme="dark"
+      :items="navItems"
+      />`,
+  })
+)
+.add(
+  'Premium',
+  () => ({
+    mixins: [dataMixin],
+    template: `<navigation
+      theme="premium"
+      :items="navItems"
+      />`,
+  })
+)
