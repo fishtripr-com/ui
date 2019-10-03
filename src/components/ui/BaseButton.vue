749c734235ba15eaad0ffb04112c908aff379a3f
Kevin <kevin.tassi@gmail.com>
2019-10-03 16:36:41 +0200

    Refactor design & logic button

--- src/components/ui/BaseButton.vue
+++ src/components/ui/BaseButton.vue
@@ -1,205 +1,217 @@
 <template>
-  <div
-    :class="['base-button', size, type, { disabled: disabled, white: white }]"
+  <button
+    class="base-button"
+    :class="buttonClasses"
+    :style="buttonStyle"
     @click="onClick"
   >
-    <span class="base-button_text">
-      <!-- <clip-loader class='base-button_loader'
-                   :loading='loading'
-                   :color='colorLoader'
-                   size='15px'/> -->
-      <div v-if="loading" class="lds-ring">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-
-      <!-- <fly-icon v-if='(icon !== "none") && !loading'
-                :icon='icon'
-                :color='type === "plain" ? "#353745" : "#fff"'
-                :width='15'
-                :height='15'
-                :class='label || $slots.default ? "base-button_loader" : "base-button_loader-alone"'/> -->
-
-      <template v-if="label && !$slots.default">
-        {{ label }}
-      </template>
-
-      <slot v-if="!label && $slots.default" />
-    </span>
-  </div>
+    <div v-if="isLoading || $slots.icon" class="icon-wrapper">
+      <loader v-if="isLoading" :mode="loaderMode" size="xs"/>
+      <slot v-else-if="$slots.icon" name="icon"/>
+    </div>
+    <p class="text">
+      {{ label }}
+    </p>
+  </button>
 </template>
 
 <script>
-// import ClipLoader from 'vue-spinner/src/ClipLoader.vue'
+import { COLORS } from '@fishtripr/constants'
+import Loader from '../loaders/DefaultLoader'
 
 export default {
   name: 'base-button',
+  components: { Loader },
   props: {
-    label: {
-      type: String,
-      default: '',
-    },
+    label: { type: String, default: '' },
     type: {
       type: String,
-      default: 'primary',
+      default: 'default',
       validator: value => {
         return [
-          'text',
-          'plain',
+          'default',
           'primary',
+          'premium',
           'success',
           'danger',
           'info',
           'warning',
-          'premium',
+          'text',
         ].includes(value)
       },
     },
     size: {
       type: String,
-      default: 'custom',
-      validator: value => {
-        return ['s', 'm', 'custom'].includes(value)
-      },
+      default: 'm',
+      validator: value => ['xxs', 'xs', 's', 'm', 'l', 'xl', 'xxl'].includes(value),
     },
-    white: {
-      type: Boolean,
-      default: false,
-    },
-    loading: {
-      type: Boolean,
-      default: false,
-    },
-    disabled: {
-      type: Boolean,
-      default: false,
-    },
-    icon: {
-      type: String,
-      validator: value => {
-        return (
-          [
-            'none',
-            'discover',
-            'plan',
-            'book',
-            'mapPointer',
-            'mapPointerIn',
-            'user',
-            'time',
-            'phone',
-            'quote',
-            'facebook',
-            'twitter',
-            'instagram',
-            'pinterest',
-            'people',
-            'chat',
-            'hook',
-            'fin',
-            'dumbbell',
-            'cross',
-            'check',
-            'arrowRight',
-            'triangleBottom',
-            'correct',
-            'error',
-            'padlock',
-            'burgerNav',
-          ].includes(value) && false
-        )
-      },
-    },
+    isLoading: { type: Boolean, default: false },
+    isDisabled: { type: Boolean, default: false },
+    radiusless: { type: Boolean, default: false },
   },
   computed: {
-    colorLoader() {
-      if (this.type === 'text') {
-        return '#56D2AB'
-      } else if (this.type === 'plain') {
-        return '#353745'
-      } else {
-        return 'white'
+    buttonStyle() {
+      switch (this.type) {
+        case 'default':
+          return {
+            backgroundColor: 'transparent',
+            boxShadow: 'none'
+          }
+        case 'primary':
+          return {
+            backgroundColor: COLORS.PRIMARY.GREEN,
+            borderColor: COLORS.PRIMARY.GREEN,
+            color: COLORS.BACKGROUND.WHITE
+          }
+        case 'premium':
+          return {
+            backgroundColor: COLORS.PRIMARY.PURPLE,
+            borderColor: COLORS.PRIMARY.PURPLE,
+            color: COLORS.BACKGROUND.WHITE
+          }
+        case 'success':
+          return {
+            backgroundColor: COLORS.STATE.SUCCESS,
+            borderColor: COLORS.STATE.SUCCESS,
+            color: COLORS.BACKGROUND.WHITE
+          }
+        case 'danger':
+          return {
+            backgroundColor: COLORS.STATE.DANGER,
+            borderColor: COLORS.STATE.DANGER,
+            color: COLORS.BACKGROUND.WHITE
+          }
+        case 'info':
+          return {
+            backgroundColor: COLORS.STATE.INFO,
+            borderColor: COLORS.STATE.INFO,
+            color: COLORS.BACKGROUND.WHITE
+          }
+        case 'warning':
+          return {
+            backgroundColor: COLORS.STATE.WARNING,
+            borderColor: COLORS.STATE.WARNING,
+            color: COLORS.BACKGROUND.WHITE
+          }
+        case 'text':
+          return {
+            backgroundColor: 'transparent',
+            border: 'none',
+          }
       }
     },
+    buttonSizeClasses() {
+      switch (this.size) {
+        case 'xxs':
+          return {
+            'xxs': true,
+            'txt-6': true,
+            'px-3': true,
+            'py-1': true
+          }
+        case 'xs':
+          return {
+            'xs': true,
+            'txt-5': true,
+            'px-5': true,
+            'py-2': true
+          }
+        case 's':
+          return {
+            's': true,
+            'txt-4': true,
+            'px-6': true,
+            'py-3': true
+          }
+        case 'm':
+          return {
+            'm': true,
+            'txt-3': true,
+            'px-6': true,
+            'py-3': true
+          }
+        case 'l':
+          return {
+            'l': true,
+            'txt-2': true,
+            'px-7': true,
+            'py-4': true
+          }
+        case 'xl':
+          return {
+            'xl': true,
+            'txt-2': true,
+            'px-8': true,
+            'py-5': true
+          }
+        case 'xxl':
+          return {
+            'xxl': true,
+            'txt-1': true,
+            'px-10': true,
+            'py-6': true
+          }
+      }
+    },
+    buttonClasses() {
+      return {
+        ...this.buttonSizeClasses,
+        disabled: this.isDisabled,
+        radiusless: this.radiusless
+      }
+    },
+    loaderMode() {
+      return this.type === 'default' || this.type === 'text'
+        ? 'black'
+        : 'white'
+    }
   },
   methods: {
     onClick() {
-      if (!this.loading && !this.disabled) {
+      if (!this.isLoading && !this.isDisabled) {
         this.$emit('click')
       }
     },
   },
-  // components: {
-  //   ClipLoader
-  // }
 }
 </script>
 
 <style lang="scss" scoped>
-@import '../../assets/style/index';
-
-/* css loader */
-.lds-ring {
-  display: inline-block;
-  position: relative;
-  width: 30px;
-  height: 30px;
-}
-.lds-ring div {
-  box-sizing: border-box;
-  display: block;
-  position: absolute;
-  width: 21px;
-  height: 21px;
-  margin: 2px;
-  border: 2px solid #fff;
-  border-radius: 50%;
-  animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
-  border-color: #fff transparent transparent transparent;
-}
-.lds-ring div:nth-child(1) {
-  animation-delay: -0.45s;
-}
-.lds-ring div:nth-child(2) {
-  animation-delay: -0.3s;
-}
-.lds-ring div:nth-child(3) {
-  animation-delay: -0.15s;
-}
-@keyframes lds-ring {
-  0% {
-    transform: rotate(0deg);
-  }
-  100% {
-    transform: rotate(360deg);
-  }
-}
-
-@mixin type($bgc, $textColor: $white) {
-  background-color: $bgc;
-  color: $textColor;
-  border-style: solid;
-  border-width: 1px;
-  border-color: $bgc;
-
-  &:not(.disabled):hover {
-    background-color: lighten($bgc, 4%);
-  }
-}
+@import '../../assets/style/default';
 
 .base-button {
-  display: inline-flex;
-  position: relative;
-  align-items: center;
-  justify-content: center;
-  margin: 0;
+  border-style: solid;
+  border-radius: 4px;
+  font-weight: $fw-l;
   cursor: pointer;
-  box-sizing: border-box;
-  flex: 1;
-  z-index: 10;
-  border-radius: 3px;
+  position: relative;
+  display: flex;
+  align-items: center;
+  background-color: transparent;
+
+  .icon-wrapper {
+    position: relative;
+    height: 100%;
+    min-height: 1px;
+    width: 10px;
+
+    .default-loader {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      top: 0;
+      left: -15px;
+    }
+  }
+
+  &.xxs {
+    .icon-wrapper {
+      .default-loader {
+        left: -8px;
+        transform: scale(0.7)
+      }
+    }
+  }
 
   &.disabled {
     cursor: not-allowed;
@@ -207,73 +219,8 @@ export default {
     opacity: 0.7;
   }
 
-  // SIZE
-  &.s {
-    height: auto;
-    width: initial;
-    padding: 12px 22px;
-    .base-button_text {
-      font-size: $font-size-s;
-    }
-  }
-  &.m {
-    height: auto;
-    width: initial;
-    padding: 15px 25px;
-    .base-button_text {
-      font-size: $font-size-m;
-    }
-  }
-  &.custom {
-    padding: 0 25px;
-    height: 75px;
-  }
-
-  // TYPE
-  &.primary {
-    @include type($color-background-primary);
-  }
-  &.plain {
-    @include type($color-background-plain, $color-text);
-    border-color: $color-text;
-
-    &.white {
-      color: $white;
-      border-color: $white;
-    }
-  }
-  &.success {
-    @include type($color-background-success);
-  }
-  &.danger {
-    @include type($color-background-danger);
-  }
-  &.info {
-    @include type($color-background-info);
-  }
-  &.warning {
-    @include type($color-background-warning);
-  }
-  &.premium {
-    @include type($color-background-premium);
-  }
-
-  .base-button_text {
-    display: flex;
-    align-items: center;
-    font-weight: 700;
-    white-space: nowrap;
-    font-family: $font-family;
-
-    .base-button_loader {
-      display: flex;
-      margin: -2px 10px 0 -5px;
-      max-height: 15px;
-    }
-    .base-button_loader-alone {
-      display: flex;
-      max-height: 15px;
-    }
+  &.radiusless {
+    border-radius: 0;
   }
 }
 </style>
